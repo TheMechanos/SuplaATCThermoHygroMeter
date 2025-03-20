bd152983ce6d443b6707b5f473342fc11d6589f9
Sebastian Koczur <themechanos@gmail.com>
2025-03-20 09:17:52 +0100

    Initial commit

---
+++ SuplaATCThermHygroMeter.cpp
@@ -0,0 +1,166 @@
+#include "SuplaATCThermHygroMeter.hpp"
+
+ATCThermHygroMeterController ATCThermHygroMeter::controller;
+
+ATCThermHygroMeter::ATCThermHygroMeter(unsigned long validTimeTicks)
+    : validTimeTicks(validTimeTicks)
+    , temp(TEMPERATURE_NOT_AVAILABLE)
+    , humi(HUMIDITY_NOT_AVAILABLE)
+    , lastDataReceiveTick(0) {
+
+    sendNewValue();
+    channel.setBatteryPowered(true);
+    channel.setOffline();
+    
+    controller.registerATC(this);
+}
+
+ATCThermHygroMeter::ATCThermHygroMeter(String mac, unsigned long validTimeTicks)
+    : ATCThermHygroMeter(validTimeTicks) {
+    this->mac = mac;
+}
+
+ATCThermHygroMeter::ATCThermHygroMeter(Supla::Html::CustomTextParameter* param, unsigned long validTimeTicks)
+    : ATCThermHygroMeter(validTimeTicks) {
+    char ms[20];
+    memset(ms, 0x00, 20 * sizeof(char));
+
+    param->getParameterValue(ms, 20);
+    this->mac = String(ms);
+}
+
+ATCThermHygroMeter::~ATCThermHygroMeter() { controller.unregisterATC(this); }
+
+void ATCThermHygroMeter::configScan(unsigned long scanTimeMs, unsigned long scanIntervalMs) { controller.configScan(scanTimeMs, scanIntervalMs); }
+
+
+
+
+void ATCThermHygroMeter::onInit() { controller.init(); }
+
+void ATCThermHygroMeter::iterateAlways() {
+
+    controller.iterate();
+
+    if (millis() - lastDataReceiveTick > validTimeTicks && temp != TEMPERATURE_NOT_AVAILABLE && humi != HUMIDITY_NOT_AVAILABLE) {
+        temp = TEMPERATURE_NOT_AVAILABLE;
+        humi = HUMIDITY_NOT_AVAILABLE;
+        channel.setOffline();
+        sendNewValue();
+    }
+}
+
+void ATCThermHygroMeter::sendNewValue() { channel.setNewValue(temp, humi); }
+
+void ATCThermHygroMeter::onBLEResult(String mac, const NimBLEAdvertisedDevice* device) {
+    auto data = device->getServiceData(0);
+
+    mac.toUpperCase();
+    if (mac == this->mac && data.length() >= 9) {
+
+        int16_t rssiPercent = map(device->getRSSI(), RSSI_MIN, RSSI_MAX, 0, 100);
+        if (rssiPercent > 100)
+            rssiPercent = 100;
+        if (rssiPercent < 0)
+            rssiPercent = 0;
+
+        int16_t rawTemperature = (data[7] | (data[6] << 8));
+
+        this->temp = rawTemperature * 0.1;
+        this->humi = (float)(data[8]);
+        int bat = data[9];
+
+        channel.setBatteryLevel(bat);
+        channel.setBridgeSignalStrength(rssiPercent);
+        channel.setOnline();
+        sendNewValue();
+
+        lastDataReceiveTick = millis();
+
+        SUPLA_LOG_DEBUG("BLE: Received from [%s] = Temp: %0.1f°C, Humi: %0.0f%%, Bat: %0.0f%% RSSI: %d->%u%%\n", mac.c_str(), temp, humi, bat, device->getRSSI(), rssiPercent);
+    }
+}
+
+
+double ATCThermHygroMeter::getTemp() { return temp; }
+
+double ATCThermHygroMeter::getHumi() { return humi; }
+
+
+
+
+
+
+ATCThermHygroMeterController::ATCThermHygroMeterController()
+    : scanTimeMs(5000)
+    , scanIntervalMs(120000) {
+    for (size_t q = 0; q < SUPLA_ATC_THERMHYGROMETER_MAX_SENSORS; q++)
+        atcs[q] = nullptr;
+}
+
+void ATCThermHygroMeterController::configScan(unsigned long scanTimeMs, unsigned long scanIntervalMs) {
+    this->scanTimeMs = scanTimeMs;
+    this->scanIntervalMs = scanIntervalMs;
+}
+
+void ATCThermHygroMeterController::init() {
+    static bool inited = false;
+    if (inited)
+        return;
+
+    NimBLEDevice::setScanDuplicateCacheSize(200);
+    NimBLEDevice::init(Supla::RegisterDevice::getName());
+    pBLEScan = NimBLEDevice::getScan();
+    pBLEScan->setScanCallbacks(this, false);
+    pBLEScan->setActiveScan(true);
+    pBLEScan->setInterval(100);
+    pBLEScan->setWindow(50);
+    pBLEScan->setMaxResults(0);
+
+    inited = true;
+}
+
+bool ATCThermHygroMeterController::registerATC(ATCThermHygroMeter* atc) {
+    for (size_t q = 0; q < SUPLA_ATC_THERMHYGROMETER_MAX_SENSORS; q++) {
+        if (atcs[q] == nullptr) {
+            atcs[q] = atc;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool ATCThermHygroMeterController::unregisterATC(ATCThermHygroMeter* atc) {
+    for (size_t q = 0; q < SUPLA_ATC_THERMHYGROMETER_MAX_SENSORS; q++) {
+        if (atcs[q] == atc) {
+            atcs[q] = nullptr;
+            return true;
+        }
+    }
+    return false;
+}
+
+void ATCThermHygroMeterController::iterate() {
+
+    if (SuplaDevice.getDeviceMode() == Supla::DeviceMode::DEVICE_MODE_NORMAL) {
+        static unsigned long lastScanTime = 0;
+        if (millis() - lastScanTime > scanIntervalMs) {
+            lastScanTime = millis();
+            pBLEScan->start(scanTimeMs, false, true);
+        }
+    }
+}
+
+void ATCThermHygroMeterController::onResult(const NimBLEAdvertisedDevice* advertisedDevice) {
+
+    // Serial.printf("BLE: Device result: %s\n", advertisedDevice->toString().c_str());
+    // Serial.printf("BLE: onResult\n");
+
+    if (advertisedDevice->haveServiceData()) {
+        String macAddress = advertisedDevice->getAddress().toString().c_str();
+
+        for (size_t q = 0; q < SUPLA_ATC_THERMHYGROMETER_MAX_SENSORS; q++)
+            if (atcs[q] != nullptr)
+                atcs[q]->onBLEResult(macAddress, advertisedDevice);
+    }
+}
